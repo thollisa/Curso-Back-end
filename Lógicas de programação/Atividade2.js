4cd34a636c9a76b49f1eb220b9788fd420418b98
thollisa <125307896+thollisa@users.noreply.github.com>
2023-03-01 14:44:54 -0300

    Delete Atividade2.js

--- Lógicas de programação/Atividade2.js
+++
@@ -1,6 +0,0 @@
-function AnosEmDias(){
- IdadeDays= idade*365
- return (AnosEmDias)
-}
-var idade=prompt(`Qual a sua idade? ${idade}`)
- console.log (`Idade anos: ${idade} \n Idade dias ${AnosEmDias(idade)} "dias`);
