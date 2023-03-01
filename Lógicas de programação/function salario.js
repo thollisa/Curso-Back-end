23801d688e9c257aef8b409df2574a0b58cccd4b
thollisa <125307896+thollisa@users.noreply.github.com>
2023-03-01 15:02:38 -0300

    Add files via upload

---
+++ Lógicas de programação/function salario.js
@@ -0,0 +1,7 @@
+function salario(){
+   salario= horas*reais
+   return (salario)
+}
+var reais= prompt(`Quantos reais voce por hora? ${reais}`);
+var horas= prompt (`Quantas horas trabalhadas? ${horas}`);
+ salario (`Salario igual a : ${salario}`)
